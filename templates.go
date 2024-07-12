ebe011a1a3c6058090f2cf98ab01469f76ee86d4
JaTynan <jtynan@outlook.com.au>
2024-07-12 13:08:52 +1000

    playing with loading templates from files and within folders

--- templates.go
+++ templates.go
@@ -1,36 +1,60 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"os"
-	"strings"
+	"text/template"
 )
 
 func main() {
-	name := os.Args[1] // name is the string we enter after running the program.
-	fmt.Println(os.Args[0])
-	fmt.Println(os.Args[1])
 
-	str := fmt.Sprint(`
-	<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<title>Hello World!</title>
-		</head>
-		<body>
-			<h1>` + name + `</h1>
-		</body>
-	</html>
-	`)
+	// from package template we are going to parse files, and we provide file name.
+	// ParseFiles can take multiple strings, and returned pointers to parsed templates and errors.
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	nf, err := os.Create("index.html")
 	if err != nil {
-		log.Fatal("error creating file", err)
+		log.Println("Error creating index.html", err)
 	}
 	defer nf.Close()
 
-	io.Copy(nf, strings.NewReader(str))
+	// we are asking for result of file parsing, using pointer 'nil'
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//
+	// Now we will parse multiple files at once.
+	tpl, err = tpl.ParseFiles("one.gohtml", "two.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// now we will execute templates with template names by using ExecuteTemplate
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//
+	// We can instead parse a whole folder of *.gohtml using ParseGlob
+	// Protip, parse all of your templates in one go for performance
+	tplGlob, errGlob := template.ParseGlob("templates/*.gohtml")
+	if errGlob != nil {
+		log.Fatalln(errGlob)
+	}
+
+	// now we execute to see one of the templates.
+	errGlob = tplGlob.Execute(os.Stdout, nil)
+	if errGlob != nil {
+		log.Fatalln(errGlob)
+	}
 }
